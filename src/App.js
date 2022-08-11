1c3abc9dec871e418fbd6693e16072e492d6f0a1
Ramanan <subram+5@simplelogin.co>
2022-08-11 11:16:23 +0200

    Changed to HasnRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/NavbarElement";
 import GlobalStyle from "./globalStyles";
 import { Home } from "./pages/HomePage/Home";
